75b597a101d3dff4e3511e0e955f9c28f2c89d32
alexHooper <alexmohack@gmail.com>
2019-07-05 11:56:10 +0530

    use twofa setup

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -4,10 +4,14 @@ from django.urls import path, include
 from two_factor.urls import urlpatterns as tf_urls
 
 from two_factor.admin import AdminSiteOTPRequired
+from django_otp.admin import OTPAdminSite
+from two_factor.gateways.twilio.urls import urlpatterns as tf_twilio_urls
 
-admin.site.__class__ = AdminSiteOTPRequired
+# admin.site.__class__ = AdminSiteOTPRequired
+admin.site.__class__ = OTPAdminSite
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(tf_urls))
+    path('', include(tf_urls)),
+    path('', include(tf_twilio_urls)),
 ]
