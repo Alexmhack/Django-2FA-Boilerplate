beb53473edd8aa99f0c7516c5bca69dd77a08bac
alexHooper <alexmohack@gmail.com>
2019-07-05 10:59:40 +0530

    add twofa urls

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from two_factor.urls import urlpatterns as tf_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(tf_urls))
 ]
